bd0f7b272250fc07360ad7aa1f53b02673180be3
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-12-15 15:55:47 -0300

    1

--- src/app/payments/columns.tsx
+++ src/app/payments/columns.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { type ColumnDef } from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export type Payment = {
   id: string;
   amount: number;
@@ -18,10 +27,58 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-4 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+  // ...
 ];
--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -2,24 +2,53 @@ import { type Payment, columns } from "./columns";
 import { DataTable } from "./data-table";
 
 async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
+  // nextjs 13 fetching api from our api folder/payments
+  const payments: Payment[] = [
+    {
+      id: "728ed52s",
+      amount: 200,
+      status: "succesful",
+      email: "mejiabrayan@protonmail..com",
+    },
     {
       id: "728ed52f",
-      amount: 100,
+      amount: 300,
       status: "pending",
-      email: "m@example.com",
+      email: "kevinf@example.com",
     },
-    // ...
+    {
+      id: "728ed52d",
+      amount: 450,
+      status: "success",
+      email: "johndoe@example.com",
+    },
+    {
+      id: "728ed52v",
+      amount: 100,
+      status: "failed",
+      email: "ianjones@example.com",
+    },
   ];
+
+  const data = payments;
+  return data;
 }
 
 export default async function DemoPage() {
   const data = await getData();
 
   return (
-    <div className="container mx-auto py-10">
+    <section className="container flex flex-col  gap-6 py-8 text-black ring-1 ring-gray-900/10 md:max-w-[64rem]  md:py-12 lg:py-24">
+      <div className="relative mx-auto flex w-full flex-col gap-4 md:max-w-[58rem]">
+        <h2 className="font-heading text-3xl leading-[1.1] text-black sm:text-3xl md:text-6xl ">
+          Data-Table Demo
+        </h2>
+        <p className="text-muted-foreground max-w-[85%] leading-normal sm:text-lg sm:leading-7">
+          using Shadcn ui and tanstack{" "}
+        </p>
+      </div>
+
       <DataTable columns={columns} data={data} />
-    </div>
+    </section>
   );
 }
